ea423d13feec10c0f15ff56a3685a2305010339d
Franklin Anto <franklin@jsam.in>
2025-02-03 22:43:50 +0530

    7-Learened One and Two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../shared/side-bar/side-bar.component';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app';
+  classNames = 'p-5 text-6xl';
+  name = 'Franklin Anto';
 }
