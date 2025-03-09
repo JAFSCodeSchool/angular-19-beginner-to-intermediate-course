5e12771a1817163a88b81535afa0b06bd40d98c9
Franklin Anto <franklin@jsam.in>
2025-03-09 12:21:43 +0530

    9-Updated sidebar menu

--- src/shared/components/side-bar/side-bar.component.ts
+++ src/shared/components/side-bar/side-bar.component.ts
@@ -7,4 +7,10 @@ import { SideBarItemComponent } from './side-bar-item/side-bar-item.component';
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
-export class SideBarComponent {}
+export class SideBarComponent {
+  routes = [
+    { name: 'Home', path: '/' },
+    { name: 'Notes', path: '/notes' },
+    { name: 'Trash', path: '/trash' },
+  ];
+}
