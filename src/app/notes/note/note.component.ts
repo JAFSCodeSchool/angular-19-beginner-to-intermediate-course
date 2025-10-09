f2f01e74c71edfc91de1cb58b20c63c697e234dd
Franklin Anto <franklin@jsam.in>
2025-10-09 07:07:07 +0530

    14-learning input-and-model-signals

--- src/app/notes/note/note.component.ts
+++ src/app/notes/note/note.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, model, OnInit } from '@angular/core';
+import { Note } from '../../../shared/models/note.model';
 
 @Component({
   selector: 'app-note',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './note.component.html',
   styleUrl: './note.component.css'
 })
-export class NoteComponent {
+export class NoteComponent implements OnInit{
+  
+  note = model.required<Note>()
+  index = input(-1, {transform:this.toSNo, alias:'sno'});
+  // sNo = computed(()=>this.index()+1)
 
+  toSNo(input:number):number{
+    return input+1;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.note.update((x)=> {
+        x.title = 'test';
+        return x;
+      })
+    }, 2000);
+  }
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { NoteComponent } from './note/note.component';
+import { Note } from '../../shared/models/note.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-notes',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NoteComponent, JsonPipe],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.css'
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit{
+  
+  notes = signal<Note[]>([
+    {
+      id: 1,
+      title:'Some Title',
+      details:'some details to show'
+    },
+    {
+      id: 2,
+      title:'Some Title 2',
+      details:'some details to show 2'
+    }
+  ]);
 
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.notes[0].title = 'test';
+    // }, 2000);
+  }
 }
