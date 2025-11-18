dea6ad3826b8c3806511c6d14729c1592f3fd77c
Franklin Anto <franklin@jsam.in>
2025-11-18 06:00:12 +0530

    Angular Template driven forms

--- src/app/notes/note/note.component.ts
+++ src/app/notes/note/note.component.ts
@@ -8,21 +8,8 @@ import { Note } from '../../../shared/models/note.model';
   styleUrl: './note.component.css'
 })
 export class NoteComponent implements OnInit{
-  
-  note = model.required<Note>()
-  index = input(-1, {transform:this.toSNo, alias:'sno'});
-  // sNo = computed(()=>this.index()+1)
-
-  toSNo(input:number):number{
-    return input+1;
-  }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.note.update((x)=> {
-        x.title = 'test';
-        return x;
-      })
-    }, 2000);
+    
   }
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,33 +1,32 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { NoteComponent } from './note/note.component';
 import { Note } from '../../shared/models/note.model';
 import { JsonPipe } from '@angular/common';
+import { Form, FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
-  imports: [RouterOutlet, NoteComponent, JsonPipe],
+  imports: [RouterOutlet, NoteComponent, JsonPipe, FormsModule],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.css'
 })
-export class NotesComponent implements OnInit{
-  
-  notes = signal<Note[]>([
-    {
-      id: 1,
-      title:'Some Title',
-      details:'some details to show'
-    },
-    {
-      id: 2,
-      title:'Some Title 2',
-      details:'some details to show 2'
-    }
-  ]);
+export class NotesComponent implements AfterViewInit {
 
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   this.notes[0].title = 'test';
-    // }, 2000);
+  @ViewChild('detailsForm') detailsForm: NgForm | undefined = undefined;
+
+  details = {
+    name: '',
+    age: 0,
+    gender: 'm'
+  };
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  submit(form: NgForm) {
+    console.log(form.value)
+    form.reset();
   }
 }
